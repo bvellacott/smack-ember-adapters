91c9b8fca44a012f66b7eb6368010674debaf84c
Benjamin Vellacott <bmvellacott@gmail.com>
2016-06-14 01:00:25 +0100

    added code in hooks

--- app/adapters/ls-adapter.js
+++ app/adapters/ls-adapter.js
@@ -1,4 +1,5 @@
 import LSAdapter from 'smack-ember-adapters/adapters/ls-adapter';
 import Where from 'npm:where-clause-evaluate';
+import compiler from 'npm:smack-js-compiler';
 
 export default LSAdapter;
